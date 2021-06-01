d22e8236478ad2964787714391be9a44e5441143
Abou Zuhayr <you@example.com>
2021-06-01 20:18:07 +0530

    test of the data output

--- visualize_data.py
+++ visualize_data.py
@@ -8,33 +8,65 @@ import seaborn as sns
 file_no_emergency = pd.read_csv('trip_info_no_emergency.csv')
 file_emergency = pd.read_csv('trip_info_emergency.csv')
 
-print(file_no_emergency.shape)
-print(file_emergency.shape)
-
 file_emergency = file_emergency.drop(['departLane', 'departPos', 'departSpeed',
                                      'arrivalLane', 'arrivalSpeed', 'rerouteNo',  'devices', 'vaporized', 'arrivalPos'], axis=1)
 file_no_emergency = file_no_emergency.drop(
     ['departLane', 'departPos', 'departSpeed', 'arrivalLane', 'arrivalSpeed', 'rerouteNo',  'devices', 'vaporized', 'arrivalPos'], axis=1)
 
 
-file_emergency['avg_speed'] = file_emergency['routeLength']/ file_emergency['duration']
+file_emergency['avg_speed'] = file_emergency['routeLength'] / \
+    file_emergency['duration']
 
-file_no_emergency['avg_speed'] = file_no_emergency['routeLength']/ file_no_emergency['duration']
+file_no_emergency['avg_speed'] = file_no_emergency['routeLength'] / \
+    file_no_emergency['duration']
 
+file_emergency = file_emergency.replace(
+    {'vType': {'DEFAULT_VEHTYPE': 'Normal Vehicles', 'bus': 'Emergency Vehicles'}})
+file_no_emergency = file_no_emergency.replace(
+    {'vType': {'DEFAULT_VEHTYPE': 'Normal Vehicles', 'bus': 'Emergency Vehicles'}})
 
-for val in  file_emergency['avg_speed']:
-    if(val<0):
-        print(val)
+avg_duration_emergency_in_emergency = (file_emergency.query('vType == "Emergency Vehicles"')['duration'].sum())/file_emergency.query('vType == "Emergency Vehicles"').shape[0]
+avg_duration_no_emergency_in_emergency = (file_emergency.query('vType == "Normal Vehicles"')['duration'].sum())/file_emergency.query('vType == "Normal Vehicles"').shape[0]
+
+avg_duration_emergency_in_no_emergency = (file_no_emergency.query('vType == "Emergency Vehicles"')['duration'].sum())/file_no_emergency.query('vType == "Emergency Vehicles"').shape[0]
+avg_duration_no_emergency_in_no_emergency = (file_no_emergency.query('vType == "Normal Vehicles"')['duration'].sum())/file_no_emergency.query('vType == "Normal Vehicles"').shape[0]
+
+print(avg_duration_emergency_in_emergency)
+print(avg_duration_no_emergency_in_emergency)
+
+print(avg_duration_emergency_in_no_emergency)
+print(avg_duration_no_emergency_in_no_emergency)
+
 
 
 sns.set(font_scale=1.5)
-sns.lmplot(x='timeLoss', y='avg_speed', data = file_no_emergency, 
-           hue='vType')
+sns.lmplot(x='timeLoss', y='avg_speed', data=file_emergency,
+           hue='vType').set(title='Priority to Emergency Vehicles')
 plt.gcf().set_size_inches(12, 8)
 plt.ylabel("Average Speed")
 plt.xlabel("Loss of Time")
 plt.show()
 
+sns.set(font_scale=1.5)
+sns.lmplot(x='timeLoss', y='avg_speed', data=file_no_emergency,
+           hue='vType').set(title='No Priority to Emergency Vehicles')
+plt.gcf().set_size_inches(12, 8)
+plt.ylabel("Average Speed")
+plt.xlabel("Loss of Time")
+plt.show()
 
-print(file_no_emergency.head)
-print(file_emergency.head)
+sns.set(font_scale=1.5)
+sns.jointplot(x='depart', y='timeLoss', data=file_emergency,
+           hue='vType')
+plt.gcf().set_size_inches(12, 8)
+plt.ylabel("Time of Departure")
+plt.xlabel("Loss of Time")
+plt.show()
+
+sns.set(font_scale=1.5)
+sns.jointplot(x='depart', y='timeLoss', data=file_no_emergency,
+           hue='vType')
+plt.gcf().set_size_inches(12, 8)
+plt.ylabel("Time of Departure")
+plt.xlabel("Loss of Time")
+plt.show()
